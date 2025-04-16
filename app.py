a41b22075d6c50b8a088e0ae86a14d14165db764
Harsh Gupta <gladiator098123@gmail.com>
2025-04-17 02:25:11 +0530

    Final Update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 # --- Page Setup (Should be first Streamlit command) ---
 st.set_page_config(layout='wide', page_title="AI Hedge Fund", page_icon=":moneybag:")
-import install_talib
 
 # --- Core Imports ---
 import time
